bcb6bd1b1536a2c096feef0e6fb73453d31e3531
OliveAlila <156780234+OliveAlila@users.noreply.github.com>
2026-08-10 23:45:33 +0300

    feat: add React foundations project structure

---
+++ phase-1-react-fundamentals/01-todo-app/src/components/Todo.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Todo = () => {
+  return (
+    <div>
+      import TodoWrapper from './TodoWrapper'
+    </div>
+  )
+}
+
+export default Todo
---
+++ phase-1-react-fundamentals/01-todo-app/src/components/TodoWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TodoWrapper = () => {
+  return (
+    <div className='TodoWrapper'>
+      <TodoForm />
+    </div>
+  )
+}
+
+export default TodoWrapper
