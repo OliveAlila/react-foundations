1b7c1e67d0ae7eeb524adac766702dc920d1cc7c
OliveAlila <156780234+OliveAlila@users.noreply.github.com>
2026-09-09 23:40:09 +0300

    GithubExplorer: types\github.ts

---
+++ phase-2-APIs/02-github-explorer/src/types/github.ts
@@ -0,0 +1,22 @@
+export interface GitHubUser {
+  login: string;
+  name: string | null;
+  avatar_url: string;
+  bio: string | null;
+  html_url: string;
+  followers: number;
+  following: number;
+  public_repos: number;
+  created_at: string;
+}
+
+export interface GitHubRepository {
+  id: number;
+  name: string;
+  description: string | null;
+  html_url: string;
+  stargazers_count: number;
+  forks_count: number;
+  language: string | null;
+  updated_at: string;
+}
